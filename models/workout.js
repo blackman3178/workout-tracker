e6dd0808160d8be76f662d3a84444bc28a7fd5fc
Noah Blackman <nablackm@usc.edu>
2021-11-17 20:18:27 -0500

    added backend

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise type:",
+      },
+      name: {
+        type: String,
+        required: "Exercise name:",
+      },
+      distance: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Duration:",
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
