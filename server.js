75aba2962f56310e05747cdf269ef0d598181c93
Noah Blackman <nablackm@usc.edu>
2021-11-17 20:36:58 -0500

    heroku & mongodb atlas

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
